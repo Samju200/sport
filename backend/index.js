6deada39773f2579346ff6d25a9f6dfb5ab0606b
Adeyemi juwon <samju7778@gmail.com>
2022-03-30 19:12:59 +0100

    make it better

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+const path = require('path');
 const userRoute = require('./routes/userRoute');
 const authRoute = require('./routes/authRoute');
 const { response } = require('express');
@@ -24,7 +24,12 @@ app.use(express.json());
 app.use('/', authRoute);
 
 app.use('/users', userRoute);
-
+if ((process.env.NODE_ENV = 'production')) {
+  app.use(express.static(path.join('sport/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'sport', 'build', 'index.html'));
+  });
+}
 app.listen(process.env.PORT || 5000, () => {
   console.log('Backend server is running!');
 });
