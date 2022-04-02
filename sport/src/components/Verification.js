3e5e8627870e66bf0f5f2ec00c430ea63d967fc0
Adeyemi juwon <samju7778@gmail.com>
2022-04-02 22:01:05 +0100

    working

--- sport/src/components/Verification.js
+++ sport/src/components/Verification.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import Verify from './Verify';
